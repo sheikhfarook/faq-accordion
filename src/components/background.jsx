8505f82fcf54e551d93a32a1162e1577c0a576c8
Farook <mohammedfarook085@gmail.com>
2023-12-22 12:11:09 +0530

    fix the tailwind-css

---
+++ src/components/background.jsx
@@ -0,0 +1,11 @@
+import frame from "../assets/frame 4.svg";
+
+const background = () => {
+  return (
+    <div>
+      <img src={frame} alt="" />
+    </div>
+  );
+};
+
+export default background;
